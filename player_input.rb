8c2a5b25a173853377e9ee1860f9809ac1452771
Amanda <cheng.1319@osu.edu>
2020-05-25 21:12:44 -0400

    player_input

---
+++ player_input.rb
@@ -0,0 +1,18 @@
+
+#Assume player starts at 1 for Player 1
+player = 1
+num_players = 3
+puts "End of Player"
+while player < num_players
+  player +=1
+  puts "Player #{player}'s Turn."
+  puts "Pick a set of 3 cards. Type the card #"
+
+  card1 = gets
+  card2 = gets
+  card3 = gets
+
+  #call check set method here
+end
+
+#If user is
